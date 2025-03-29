e49fb8c56c8b60e4619d2397e629b376bbef31d4
Hemanth411-stack <nagalahemanth411@gmail.com>
2025-03-29 20:48:26 +0530

    added

---
+++ app/(root)/interview/page.jsx
@@ -0,0 +1,19 @@
+import Agent from "../../../components/Agent";
+// import { getCurrentUser } from "@/lib/actions/auth.action";
+
+const Page = async () => {
+//   const user = await getCurrentUser();
+
+  return (
+    <>
+      <h3>Interview generation</h3>
+
+      <Agent
+        
+        type="generate"
+      />
+    </>
+  );
+};
+
+export default Page;
